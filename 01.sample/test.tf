8da40269ea730e187a64e7f54d8c49db593de68d
navaneetha <navani91@gmail.com>
2023-04-28 12:28:43 +0100

    learn-terraform

--- 01.sample/test.tf
+++ 01.sample/test.tf
@@ -1,8 +1,8 @@
-resource"aws_instance" "ec2"{
-ami = ami-0a017d8ceb274537d
-instance_type = "t3.micro"
-vpc_security_group_ids = ["sg-06e0c53ec37e1d493"]
-tags = {
+resource "aws_instance" "ec2" {
+ ami = "0a017d8ceb274537d"
+ instance_type = "t3.micro"
+ vpc_security_group_ids = ["sg-06e0c53ec37e1d493"]
+ tags = {
   Name = "test"
 
 }
