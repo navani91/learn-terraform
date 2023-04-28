7d1af821927efee7cba36cdceb83150fb30f3eed
navaneetha <navani91@gmail.com>
2023-04-28 15:29:59 +0100

    learn-terraform

--- 04.variable/var.tf
+++ 04.variable/var.tf
@@ -2,7 +2,17 @@ variable"sample" {
  default "hello world"
 }
 
-
 output"sample" {
  value = var.sample
+}
+variable "sample1"{}
+
+OUTPUT "sample1" {
+ value = var.smaple1
+
+}
+varible "cli"{}
+
+output "cli"{
+ value = var.cli
 }
