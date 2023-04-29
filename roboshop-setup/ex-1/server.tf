fd065cd8efa7c699f4f54e4630f9f068ba8c951b
navaneetha <navani91@gmail.com>
2023-04-29 14:54:37 +0100

    learn-terraform

--- roboshop-setup/ex-1/server.tf
+++ roboshop-setup/ex-1/server.tf
@@ -1,9 +1,21 @@
-resource "aws_instance" "frontend {
+resource "aws_instance" "frontend" {
  ami = "0a017d8ceb274537d"
  instance_type = "t3.micro"
  vpc_security_group_ids = ["sg-06e0c53ec37e1d493"]
  tags = {
   Name = "frontend"
+
+}
+
+
+}
+
+resource "aws_instance" "redis" {
+ ami = "0a017d8ceb274537d"
+ instance_type = "t3.micro"
+ vpc_security_group_ids = ["sg-06e0c53ec37e1d493"]
+ tags = {
+  Name = "redis"
 
 }
 
@@ -28,30 +40,6 @@ resource "aws_instance" "catalogue" {
  vpc_security_group_ids = ["sg-06e0c53ec37e1d493"]
  tags = {
   Name = "catalogue"
-
-}
-
-
-}
-
-resource "aws_instance" "user" {
- ami = "0a017d8ceb274537d"
- instance_type = "t3.micro"
- vpc_security_group_ids = ["sg-06e0c53ec37e1d493"]
- tags = {
-  Name = "user"
-
-}
-
-
-}
-
-resource "aws_instance" "cart" {
- ami = "0a017d8ceb274537d"
- instance_type = "t3.micro"
- vpc_security_group_ids = ["sg-06e0c53ec37e1d493"]
- tags = {
-  Name = "cart"
 
 }
 
@@ -94,24 +82,36 @@ resource "aws_instance" "payment" {
 
 }
 
+resource "aws_instance" "user" {
+ ami = "0a017d8ceb274537d"
+ instance_type = "t3.micro"
+ vpc_security_group_ids = ["sg-06e0c53ec37e1d493"]
+ tags = {
+  Name = "user"
+
+}
+
+
+}
+
+resource "aws_instance" "cart" {
+ ami = "0a017d8ceb274537d"
+ instance_type = "t3.micro"
+ vpc_security_group_ids = ["sg-06e0c53ec37e1d493"]
+ tags = {
+  Name = "cart"
+
+}
+
+
+}
+
 resource "aws_instance" "shipping" {
  ami = "0a017d8ceb274537d"
  instance_type = "t3.micro"
  vpc_security_group_ids = ["sg-06e0c53ec37e1d493"]
  tags = {
   Name = "shipping"
-
-}
-
-
-}
-
-resource "aws_instance" "redis" {
- ami = "0a017d8ceb274537d"
- instance_type = "t3.micro"
- vpc_security_group_ids = ["sg-06e0c53ec37e1d493"]
- tags = {
-  Name = "redis"
 
 }
 
