0f5c0b1a161c4b26f167bf354011e14db40c964c
navaneetha <navani91@gmail.com>
2023-05-16 16:35:35 +0100

    learn-terraform

--- roboshop-setup/ex5/route53/main.tf
+++ roboshop-setup/ex5/route53/main.tf
@@ -1,9 +1,10 @@
-//resource "aws_route53_record" "frontend" {
-//  zone_id = "Z103214126L48SQW30RSR"
-//  name    = "frontend-dev.devopsb71.online"
-//  type    = "A"
-//  ttl     = 30
-//  records = [aws_instance.frontend.private_ip]
-//}
+resource "aws_route53_record" "frontend" {
+ zone_id = "Z103214126L48SQW30RSR"
+  name    = "frontend-dev.devopsb71.online"
+  type    = "A"
+  ttl     = 30
+  records = [var.private_ip]
+}
 
 variable "private_ip" {}
+variable "component" {}
