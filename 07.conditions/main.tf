a323e1ce0bcc093f5176c146f7fc60105638257f
navaneetha <navani91@gmail.com>
2023-05-16 13:14:20 +0100

    learn-terraform

--- 07.conditions/main.tf
+++ 07.conditions/main.tf
@@ -1,5 +1,14 @@
 resource "aws_instance" "test"
 ami = "0a017d8ceb274537d"
-instance_type = var.instance_type
+instance_type = var.instance_type == "" ? "t3.micro" ; var.instance_type
 
 variable "instance_type" {}
+
+variable "create_instance" {}
+
+resource "aws_instance" "test1"{
+count = toboo1(var.create_instance) ? 1 : 0
+ami = "0a017d8ceb274537d"
+instance_type = var.instance_type == "" ? "t3.micro" ; var.instance_type
+}
+
