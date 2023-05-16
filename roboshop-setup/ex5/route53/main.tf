2c50cc2ac34da083697c12e902172d1ac4862ecc
navaneetha <navani91@gmail.com>
2023-05-16 16:09:56 +0100

    learn-terraform

---
+++ roboshop-setup/ex5/route53/main.tf
@@ -0,0 +1,7 @@
+resource "aws_route53_record" "frontend" {
+  zone_id = "Z103214126L48SQW30RSR"
+  name    = "frontend-dev.devopsb71.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.frontend.private_ip]
+}
